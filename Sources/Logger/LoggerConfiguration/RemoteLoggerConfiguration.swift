fe8322cd832e5c0bf5c1e0b56957b4e69a1862eb
Tim Ellis <3098078+TimAEllis@users.noreply.github.com>
2022-05-28 15:54:19 +1000

    Add easier access to endpoint property

--- Sources/Logger/LoggerConfiguration/RemoteLoggerConfiguration.swift
+++ Sources/Logger/LoggerConfiguration/RemoteLoggerConfiguration.swift
@@ -1,13 +1,21 @@
 import Foundation
 
 open class RemoteLoggerConfiguration: LoggerConfigurationBase {
+    let remoteRecorder: RemoteLoggerRecorder
+    
 	public override init(minimumSeverity: LoggerSeverity = .warn, recorders: [LoggerRecorder] = [], synchronousMode: Bool = false) {
 		var recorders = recorders
-		recorders.append(RemoteLoggerRecorder())
+        self.remoteRecorder = RemoteLoggerRecorder()
+        recorders.append(self.remoteRecorder)
 		super.init(
 			minimumSeverity: minimumSeverity,
 			recorders: recorders,
 			synchronousMode: synchronousMode
 		)
 	}
+    
+    @discardableResult func setEndpoint(_ endpoint: URL) -> Self {
+        self.remoteRecorder.endpoint = endpoint
+        return self
+    }
 }
